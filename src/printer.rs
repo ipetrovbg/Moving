3431a0a946e8cbcc23fb0b342bbe6270947e9135
petarpetrov <ipetrovbg@gmail.com>
2023-06-12 23:10:18 +0300

    fix(print): fix too long header and description

--- src/printer.rs
+++ src/printer.rs
@@ -5,7 +5,6 @@ use crate::package::Package;
 
 pub fn print_package(package: Package, count: usize) -> Result<(), Error> {
     let printer_details = PrinterProfile::usb_builder(0x0456, 0x0808).build();
-    // We pass it to the printer
     let printer = match Printer::new(printer_details) {
         Ok(maybe_printer) => match maybe_printer {
             Some(printer) => printer,
@@ -13,6 +12,7 @@ pub fn print_package(package: Package, count: usize) -> Result<(), Error> {
         },
         Err(e) => panic!("Error: {}", e)
     };
+
     let center_instruction = Instruction::text(
         "%center%",
         Font::FontA,
@@ -28,30 +28,49 @@ pub fn print_package(package: Package, count: usize) -> Result<(), Error> {
         .replacement("%center%", "*** MOVING APP ***")
         .build();
 
+    let print_name = PrintData::builder()
+        .replacement("%center%", package.name)
+        .build();
+
+    let print_description = PrintData::builder()
+        .replacement("%center%", package.description)
+        .build();
+
+
     printer.println("").unwrap();
     printer.println("").unwrap();
     printer.println("").unwrap();
     printer.println("").unwrap();
+
     printer.instruction(&center_instruction, Some(&print_header)).unwrap();
+
     printer.println("").unwrap();
+
     printer.instruction(&center_instruction, Some(&print_title)).unwrap();
+
     printer.println("").unwrap();
     printer.println("").unwrap();
-    // printer.instruction(&center_instruction, Some(&print_nil))?;
+
     match printer.duo_table(("", ""), vec![
-        ("Nil", package.nil),
-        ("Name", package.name),
-        ("Description", package.description),
+        ("Nil", format!("[ {} ]", package.nil)),
         ("ID", package.id.to_string()),
     ]) {
         Ok(_) => (),
         Err(e) => println!("Error: {}", e)
     }
-    // printer.println("").unwrap();
-    // printer.instruction(&center_instruction, Some(&print_separator)).unwrap();
-    // printer.println("").unwrap();
-    // printer.instruction(&center_instruction, Some(&print_name))?;
-    // printer.instruction(&center_instruction, Some(&print_description))?;
+
+    printer.println("").unwrap();
+    printer.instruction(&center_instruction, Some(&print_header)).unwrap();
+    printer.println("").unwrap();
+
+    printer.instruction(&center_instruction, Some(&print_name)).unwrap();
+
+    printer.println("").unwrap();
+
+    printer.instruction(&center_instruction, Some(&print_description)).unwrap();
+
+    printer.println("").unwrap();
+
     match printer.duo_table(("", ""), vec![
         ("Created On", package.created_at.format("%d/%b/%Y").to_string()),
         ("Printed On", Utc::now().format("%d/%b/%Y").to_string()),
@@ -66,6 +85,7 @@ pub fn print_package(package: Package, count: usize) -> Result<(), Error> {
         Ok(_) => (),
         Err(e) => println!("Error: {}", e)
     }
+
     printer.println("").unwrap();
     printer.println("").unwrap();
     printer.println("").unwrap();
@@ -74,5 +94,6 @@ pub fn print_package(package: Package, count: usize) -> Result<(), Error> {
     printer.println("").unwrap();
     printer.println("").unwrap();
     printer.println("").unwrap();
+
     Ok(())
 }
